d3437e7143237b997dffcbad113751284c1a42d8
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2023-02-03 19:17:45 -0500

    modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// we received currentPhoto from the PhotoList component...
+function Modal({ currentPhoto }) {
+  // then assigned its properties in the modal
+  const { name, category, description, index } = currentPhoto;
+
+  return (
+    <div className='modalBackdrop'>
+      <div className='modalContainer'>
+        <h3 className='modalTitle'>{name}</h3>
+        <img
+          src={require(`../../assets/large/${category}/${index}.jpg`)}
+          alt='current category'
+        />
+        <p>{description}</p>
+        <button type='button'>Close this modal</button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,6 @@
 // Photo List Component (child of Gallery)
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
   const [photos] = useState([
@@ -120,9 +121,16 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(true);
+  };
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       <div className='flex-row'>
         {currentPhotos.map((image, i) => (
           <img
@@ -130,6 +138,7 @@ const PhotoList = ({ category }) => {
             src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className='img-thumbnail mx-1'
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
